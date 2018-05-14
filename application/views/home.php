f4357b885b215952b5fe97ea9b8234fa441d4600
Muhammad Luqni Baehaqi <mluqnibaehaqi@gmail.com>
2018-05-14 13:52:31 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Mushola STT-NF</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="keywords" content="free islamic templates, free, islamic, templates. website templates, prayer times for website, icons, vectors, website themes, islamic themes" />
+<meta name="description" content="Free Islamic website templates, resources, icons, vectors, prayer times for your website, and Islamic content." />
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets2/css/uncss.css">
+<script src="three/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+<link rel="shortcut icon" href="three/favicon.ico">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url(); ?>assets2/images/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url(); ?>assets2/images/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url(); ?>assets2/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="<?php echo base_url(); ?>assets2/images/ico/apple-touch-icon-57-precomposed.png"></head>
+<body>
+<header class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a> <a id="logo" class="pull-left" href="three"></a>
+      <div class="nav-collapse collapse pull-right">
+        <ul class="nav">
+          <li class="active"><a href="three">Home</a></li>
+          <li><a href="three-about">About Me</a></li>
+          <li><a href="<?php echo site_url('prayers'); ?>">Prayers</a></li>
+          <li><a href="three-templates">Templates</a></li>
+          <li><a href="three-blog">Blog</a></li>
+          <li><a href="<?php echo site_url('welcome'); ?>">Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
+<section id="slide-show">
+  <div class="slider-shadow">
+    <div id="da-slider" class="da-slider">
+      <div class="da-slide">
+        <h2>Free Masjid Templates</h2>
+        <p>Visit <a href="https://freeislamictemplates.appspot.com/">https://freeislamictemplates.appspot.com/</a></p>
+        <a href="#" class="da-link">Read more</a>
+        <div class="da-img"><img src="<?php echo base_url(); ?>assets2/images/slider/1.png" alt="Masjid Template 1" height="165" width="307"/></div>
+      </div>
+      <div class="da-slide">
+        <h2>Free Masjid Templates</h2>
+        <p>Visit <a href="https://freeislamictemplates.appspot.com/">https://freeislamictemplates.appspot.com/</a></p>
+        <a href="#" class="da-link">Read more</a>
+        <div class="da-img"><img src="<?php echo base_url(); ?>assets2/images/slider/2.png" alt="Masjid Template 2" height="165" width="307"/></div>
+      </div>
+      <nav class="da-arrows"> <span class="da-arrows-prev"></span> <span class="da-arrows-next"></span> </nav>
+    </div>
+  </div>
+</section>
+<section id="services">
+  <div class="container">
+    <div class="center">
+      <h2><a href="https://freeislamictemplates.appspot.com/">Free Islamic Templates</a></h2>
+    </div>
+    <div class="row-fluid">
+      <div class="middle-box span3">
+        <div class="media">
+          <div class="pull-left"> <i class="icon-globe icon-medium"></i> </div>
+          <div class="media-body">
+            <h4 class="media-heading">Website Templates</h4>
+            <p>There are two available, but more will be added later.</p>
+          </div>
+        </div>
+      </div>
+      <div class="middle-box span3">
+        <div class="media">
+          <div class="pull-left"> <i class="icon-thumbs-up-alt icon-medium"></i> </div>
+          <div class="media-body">
+            <h4 class="media-heading">Logos</h4>
+            <p>Coming soon.</p>
+          </div>
+        </div>
+      </div>
+      <div class="middle-box span3">
+        <div class="media">
+          <div class="pull-left"> <i class="icon-leaf icon-medium icon-rounded"></i> </div>
+          <div class="media-body">
+            <h4 class="media-heading">Prayer Times</h4>
+            <p>Prayer times added to templates.</p>
+          </div>
+        </div>
+      </div>
+            <div class="middle-box span3">
+        <div class="media">
+          <div class="pull-left"> <i class="icon-leaf icon-medium icon-rounded"></i> </div>
+          <div class="media-body">
+            <h4 class="media-heading">Prayer Times</h4>
+            <p>Prayer times added to templates.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="recent-works">
+  <div class="container">
+    <div class="center">
+      <h2>Recent Templates</h2>
+      <p class="lead">Latest items will be added below.</p>
+    </div>
+    <ul class="gallery col-4">
+      <li>
+        <div class="preview"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/thumb/item1.jpg" alt="Image" height="128" width="213">
+          <div class="overlay"> </div>
+          <div class="links"><a href="">Preview</a></div>
+        </div>
+        <div class="desc">
+          <h5>Masjid Template 1</h5>
+        </div>
+        <div id="modal-1" class="modal hide fade"> <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></a>
+          <div class="modal-body"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/full/item1.jpg" alt="Image" height="180" width="300"> </div>
+        </div>
+      </li>
+      <li>
+        <div class="preview"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/thumb/item2.jpg" alt="Image" height="128" width="213">
+          <div class="overlay"> </div>
+          <div class="links"><a href="">Preview</a></div>
+        </div>
+        <div class="desc">
+          <h5>Masjid Template 2</h5>
+        </div>
+        <div id="modal-1" class="modal hide fade"> <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></a>
+          <div class="modal-body"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/full/item2.jpg" alt="Image" height="180" width="300"> </div>
+        </div>
+      </li>
+      <li>
+        <div class="preview"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/thumb/item1.jpg" alt="Image" height="128" width="213">
+          <div class="overlay"> </div>
+          <div class="links"><a href="">Preview</a></div>
+        </div>
+        <div class="desc">
+          <h5>Masjid Template 1</h5>
+        </div>
+        <div id="modal-3" class="modal hide fade"> <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></a>
+          <div class="modal-body"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/full/item1.jpg" alt="Image" height="180" width="300"> </div>
+        </div>
+      </li>
+      <li>
+        <div class="preview"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/thumb/item2.jpg" alt="Image" height="128" width="213">
+          <div class="overlay"> </div>
+          <div class="links"><a href="">Preview</a></div>
+        </div>
+        <div class="desc">
+          <h5>Masjid Template 2</h5>
+        </div>
+        <div id="modal-4" class="modal hide fade"> <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></a>
+          <div class="modal-body"> <img src="<?php echo base_url(); ?>assets2/images/portfolio/full/item2.jpg" alt="Image" height="180" width="300"> </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+<section id="bottom" class="main"> 
+  <div class="container"> 
+    <div class="row-fluid"> 
+      <div class="span3">
+        <h4>INFO</h4>
+        <ul class="unstyled address">
+          <li> <i class="icon-home"></i><strong>Address:</strong><a href="https://plus.google.com/b/100537539023412941569/100537539023412941569/" target="_blank">Google+</a></li>
+          <li> <i class="icon-envelope"></i> <strong>Forum: </strong><a href="#">Google Group</a></li>
+          <li> <i class="icon-globe"></i> <strong>Website:</strong><a href="/">Free Islamic Templates</a></li>
+        </ul>
+      </div>
+      <div id="tweets" class="span3">
+        <h4>SITEMAP</h4>
+        <div>
+          <ul class="arrow">
+            <li><a href="three">Home</a></li>
+            <li><a href="three-about">About Me</a></li>
+            <li><a href="three-prayers">Prayers</a></li>
+            <li><a href="three-templates">Templates</a></li>
+            <li><a href="three-blog">Blog</a></li>
+            <li><a href="three-services">Services</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="archives" class="span3">
+        <h4>WEBSITE TEMPLATES</h4>
+        <div>
+          <ul class="arrow">
+            <li><a href="#">Template 1</a></li>
+            <li><a href="#">Template 2</a></li>
+            <li><a href="#">Template 3</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="span3">
+        <h4>RESOURCES</h4>
+        <div>
+          <ul class="arrow">
+            <li><a href="http://freeislamiccalligraphy.com/" target="_blank">Islamic Calligraphy</a></li>
+            <li><a href="http://www.islamicfinder.org/world.html" target="_blank">Islamic Prayer Times</a></li>
+            <li><a href="http://findicons.com/search/islamic" target="_blank">Islamic Icons 1</a></li>
+            <li><a href="http://www.iconarchive.com/tag/icon-islam" target="_blank">Islamic Icons 2</a></li>
+            <li><a href="http://www.freepik.com/free-photos-vectors/islamic" target="_blank">Islamic Vectors 1</a></li>
+            <li><a href="http://vector.me/search/islamic" target="_blank">Islamic Vectors 2</a></li>
+            <li><a href="http://www.madaniwallpaper.com/" target="_blank">Islamic Wallpaper</a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer id="footer">
+  <div class="container">
+    <div class="row-fluid">
+      <div class="footer5 span5 cp"> &copy; <a href="https://freeislamictemplates.appspot.com/">Free Islamic Templates 2014</a> . All Rights Reserved. <a href="https://plus.google.com/100537539023412941569" rel="publisher"></a>
+</div>
+      <div class="footer6 span6">
+        <ul class="social pull-right">
+          <li><a href="https://plus.google.com/b/100537539023412941569/100537539023412941569/" target="_blank"><i class="icon-google-plus"></i></a></li>
+        </ul>
+      </div>
+      <div class="span1"> <a id="gototop" class="gototop pull-right" href="#"><i class="icon-angle-up"></i></a> </div>
+    </div>
+  </div>
+</footer>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54987858-1', 'auto');
+  ga('send', 'pageview');
+
+</script><script src="<?php echo base_url(); ?>assets2/js/all.js"></script> 
+<script type="text/javascript">
+            $(function() {
+                $('#da-slider').cslider({
+                    autoplay    : true,
+                    bgincrement : 450
+                });
+            });
+        </script>
+</body>
+</html>
